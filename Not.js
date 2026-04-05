9ededc6fc367c9d2689f3bf28e6774865e8ebedb
flybird <86914612+free-as-bird@users.noreply.github.com>
2026-04-05 12:10:04 +0800

    Update Not.js

--- Not.js
+++ Not.js
@@ -7,46 +7,46 @@ hostname = notability.com
 */
 
 const response = {
-	"data": {
-		"associateAppStoreTransactions": {
-			"__typename": "SubscriptionOverview",
-			"tier": "premium",
-			"current": {
-				"__typename": "Subscription",
-				"source": "AppStoreConsumer",
-				"tier": "premium",
-				"expirationDate": 4102416000000,
-				"renewalDate": 4102417000000,
-				"gracePeriodEndDate": 4102444800000,
-				"details": {
-					"__typename": "AppStoreSubscription",
-					"originalTransactionId": "2000001147842386",
-					"productId": "com.gingerlabs.Notability.premium_subscription",
-					"overDeviceLimit": false,
-					"appStoreStatus": "active"
-				}
-			},
-			"prior": null,
-			"quotas": {
-				"__typename": "SubscriptionFeatureQuotaView=",
-				"learnQuestions": {
-					"__typename": "SubscriptionFeatureQuota",
-					"isUsageExceeded": false,
-					"usagePercentage": 0
-				},
-				"learnSummaries": {
-					"__typename": "SubscriptionFeatureQuota",
-					"isUsageExceeded": false,
-					"usagePercentage": 0
-				},
-				"liveTranscription": {
-					"__typename": "SubscriptionFeatureQuota",
-					"isUsageExceeded": false,
-					"usagePercentage": 0
-				}
-			}
-		}
-	}
+  "data" : {
+    "associateAppStoreTransactions" : {
+      "__typename" : "SubscriptionOverview",
+      "tier" : "premium",
+      "current" : {
+        "source" : "AppStoreConsumer",
+        "expirationDate" : 4102416000000,
+        "details" : {
+          "originalTransactionId" : "2000001147842386",
+          "__typename" : "AppStoreSubscription",
+          "overDeviceLimit" : false,
+          "productId" : "com.gingerlabs.Notability.premium_subscription",
+          "appStoreStatus" : "active"
+        },
+        "renewalDate" : 4102415000000,
+        "tier" : "premium",
+        "gracePeriodEndDate" : 4102444800000,
+        "__typename" : "Subscription"
+      },
+      "quotas" : {
+        "__typename" : "SubscriptionFeatureQuotaView",
+        "learnSummaries" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        },
+        "learnQuestions" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        },
+        "liveTranscription" : {
+          "isUsageExceeded" : false,
+          "usagePercentage" : 0,
+          "__typename" : "SubscriptionFeatureQuota"
+        }
+      },
+      "prior" : null
+    }
+  }
 };
 
 $done({ body: JSON.stringify(response) });
