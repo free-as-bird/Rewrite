876b660a8025b575b7209a09a4e8c282b0f78e61
flybird <86914612+free-as-bird@users.noreply.github.com>
2026-04-05 16:42:05 +0800

    Add files via upload

---
+++ Not2.js
@@ -0,0 +1,36 @@
+/*
+[rewrite_local]
+https?:\/\/notability\.com\/global url script-response-body https://raw.githubusercontent.com/free-as-bird/Rewrite/refs/heads/main/Not2.js
+
+[mitm]
+hostname = notability.com
+*/
+
+const response = {
+  "data" : {
+    "processAppleReceipt" : {
+      "__typename" : "SubscriptionResult",
+      "isClassic" : false,
+      "subscription" : {
+        "productId" : "com.gingerlabs.Notability.premium_subscription",
+        "originalTransactionId" : "2000001147842386",
+        "tier" : "premium",
+        "refundedDate" : null,
+        "refundedReason" : null,
+        "isInBillingRetryPeriod" : false,
+        "expirationDate" : "2099-12-31T23:59:59.000Z",
+        "gracePeriodExpiresAt" : null,
+        "overDeviceLimit" : false,
+        "expirationIntent" : null,
+        "__typename" : "AppStoreSubscription",
+        "user" : null,
+        "status" : "active",
+        "originalPurchaseDate" : "2026-04-05T02:24:52.000Z"
+      },
+      "error" : 0
+    }
+  }
+};
+
+
+$done({ body: JSON.stringify(response) });
